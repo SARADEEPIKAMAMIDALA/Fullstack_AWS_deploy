1d4438feef7468606cbb4335aa552f7b4791db47
SARADEEPIKAMAMIDALA <144265896+SARADEEPIKAMAMIDALA@users.noreply.github.com>
2025-09-05 20:45:48 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'https://54.204.182.189:9090/springapp1';
+const BASE_URL = 'https://3.210.216.40/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
